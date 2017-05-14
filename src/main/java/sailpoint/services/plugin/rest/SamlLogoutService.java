1d3edc06c482403bced8a0db06c9126bfdb5625f
Mario Enrico Ragucci <mario@whisperedshouts.de>
2017-05-15 01:43:57 +0200

    PoC ready implementation
    adding config options to allow SAML based Logouts
    Various fixes
    
    Signed-off-by: Mario Enrico Ragucci <mario@whisperedshouts.de>

--- src/main/java/sailpoint/services/plugin/rest/SamlLogoutService.java
+++ src/main/java/sailpoint/services/plugin/rest/SamlLogoutService.java
@@ -11,6 +11,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.log4j.Logger;
 import org.opensaml.common.SAMLObject;
 import org.opensaml.ws.message.encoder.MessageEncodingException;
 
@@ -30,15 +31,15 @@ import sailpoint.web.sso.SAMLUtil;
 @AllowAll
 public class SamlLogoutService extends BasePluginResource {
 	public static final String SQL_SESSION_QUERY = "SELECT SESSION_INDEX FROM SAML_SESSIONS WHERE ACCOUNT=?";
+	public static final Logger _logger = Logger.getLogger(SamlLogoutService.class);
+	
+	protected boolean isSamlEnabled	= false;
+    protected boolean signAuthN		= false;
 
-	protected boolean isSamlEnabled=false;
-    protected boolean signAuthN=false;
-
-
-    protected String jksLocation=null;
-    protected String jksPassword=null;
-    protected String jksAlias=null;
-    protected String storeType="JKS";
+    protected String jksLocation	= null;
+    protected String jksPassword	= null;
+    protected String jksAlias		= null;
+    protected String storeType		= "JKS";
     
 	@Override
 	public String getPluginName() {
@@ -48,64 +49,132 @@ public class SamlLogoutService extends BasePluginResource {
 	@GET
 	@Path("doLogout")
 	public String doLogout() {
-		String redirectUrl = null;
-		String sessionIndex = null;
-		String principal = null;
-		SAMLObject logoutRequest = null;
-		boolean signRequest = getSettingBool("signRedirect");
-		try {			
-			principal = getLoggedInUserName();
-			System.out.println(principal);
-			sessionIndex = returnSessionIndexFromDb(principal);
-			System.out.println(sessionIndex);
-			SAMLConfig samlConfig = getSamlConfig();
-			if(samlConfig != null) {
-				logoutRequest = SAMLUtil.buildLogoutRequest(principal, samlConfig.getEntityId(), sessionIndex);
-				if(signRequest) {
-					setKeystoreInformation();
-					sailpoint.services.x509key.JKSKeyManager jks =
-	                        sailpoint.services.x509key.JKSKeyManager.getInstance(jksLocation,jksPassword,jksPassword,jksAlias,storeType);
-					redirectUrl = SAMLUtil.buildSignedLogoutUrl(getSettingString("redirectUrl"), logoutRequest, jks.getDefaultCredential());				
-				}else {
-					redirectUrl = SAMLUtil.buildLogoutUrl(getSettingString("redirectUrl"), logoutRequest);
-				}				
-			}
-			
-		} catch (GeneralException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (MessageEncodingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		if(_logger.isDebugEnabled()) {
+			_logger.debug(String.format("ENTERING method %s()", "doLogout"));
 		}
+		
+		boolean signRequest 	 	= getSettingBool("signRedirect");
+		boolean createLogoutRequest	= getSettingBool("createLogoutRequest");
+		String redirectUrl 		 	= getSettingString("redirectUrl");		
+		
+		if(_logger.isDebugEnabled()) {
+			_logger.debug(String.format("redirectURL: %s, createLogoutRequest: %s, signRequest: %s",
+					redirectUrl, createLogoutRequest, signRequest));
+		}
+		
+		if(createLogoutRequest) {
+			String sessionIndex 	 	= null;
+			String principal 		 	= null;
+			SAMLObject logoutRequest 	= null;
+			
+			try {			
+				principal = getLoggedInUserName();
+				sessionIndex = returnSessionIndexFromDb(principal);
+				SAMLConfig samlConfig = getSamlConfig();
+				
+				if(samlConfig != null) {
+					String entityId = samlConfig.getEntityId();
+					if(entityId == null) {
+						throw new GeneralException("entityID could not be retrieved from SAML Configuration");
+					}
+					logoutRequest = SAMLUtil.buildLogoutRequest(principal, entityId, sessionIndex);
+					
+					if(signRequest) {
+						setKeystoreInformation();
+						if(!validateKeystoreInformation()) {
+							throw new GeneralException("Important Configuration Elements are missing. Were the SAML enhancements installed?");
+						}
+						
+						// Build signed logout URL
+						sailpoint.services.x509key.JKSKeyManager jks =
+		                        sailpoint.services.x509key.JKSKeyManager.getInstance(jksLocation,jksPassword,jksPassword,jksAlias,storeType);
+						redirectUrl = SAMLUtil.buildSignedLogoutUrl(redirectUrl, logoutRequest, jks.getDefaultCredential());				
+					}else {
+						// Build reqular logout URL
+						redirectUrl = SAMLUtil.buildLogoutUrl(redirectUrl, logoutRequest);
+					}				
+				} else {
+					throw new GeneralException("no SAMLConfig could be retrieved from the database. Please review your SAML configuration");
+				}
+				
+			} catch (GeneralException e) {
+				_logger.error(e.getMessage(), e);
+			} catch (SQLException e) {
+				_logger.error(e.getMessage(), e);
+			} catch (MessageEncodingException e) {
+				_logger.error(e.getMessage(), e);
+			}
+		}
+		
+		// Invalidat session and return URL
 		getSession().invalidate();
+		
+		if(_logger.isDebugEnabled()) {
+			_logger.debug(String.format("LEAVING method %s (returns: %s)", "doLogout", redirectUrl));
+		}
 		return redirectUrl;
 	}
 	
+	private boolean validateKeystoreInformation() {
+		if(_logger.isDebugEnabled()) {
+			_logger.debug(String.format("ENTERING method %s()", "validateKeystoreInformation"));
+		}
+		boolean result = false;
+		if(_logger.isDebugEnabled()) {
+			_logger.debug(String.format("jksLocation: %s, jksAlias: %s, jksPassword: %s, storeType: %s", 
+					jksLocation, jksAlias, jksPassword, storeType));
+		}
+		result = (jksLocation != null && !jksLocation.isEmpty()) ? true : false;
+		result = (jksAlias != null && !jksAlias.isEmpty()) ? true : false;
+		result = (jksPassword != null && !jksPassword.isEmpty()) ? true : false;
+		result = (storeType != null && !storeType.isEmpty()) ? true : false;
+		
+		if(_logger.isDebugEnabled()) {
+			_logger.debug(String.format("LEAVING method %s (returns: %s)", "validateKeystoreInformation", result));
+		}
+		
+		return result;
+	}
+	
 	private void setKeystoreInformation() throws GeneralException {
-		Configuration sysConfig = getContext().getConfiguration();
-        this.isSamlEnabled = sysConfig.getBoolean(Configuration.SAML_ENABLED, false);
-        this.signAuthN=sysConfig.getBoolean(ServicesSAMLConstant.CONFIG_SIGN_AUTHN, false);
-        jksLocation=sysConfig.getString(ServicesSAMLConstant.CONFIG_SP_CERT);
-        jksAlias=sysConfig.getString(ServicesSAMLConstant.CONFIG_SP_CERT_ALIAS);
-        jksPassword=sysConfig.getString(ServicesSAMLConstant.CONFIG_SP_STORE_PASS);
-        storeType=sysConfig.getString(ServicesSAMLConstant.CONFIG_SP_STORE_TYPE);
+		if(_logger.isDebugEnabled()) {
+			_logger.debug(String.format("ENTERING method %s()", "setKeystoreInformation"));
+		}
+		Configuration sysConfig;
+		sysConfig		= getContext().getConfiguration();
+        isSamlEnabled 	= sysConfig.getBoolean(Configuration.SAML_ENABLED, false);
+        signAuthN		= sysConfig.getBoolean(ServicesSAMLConstant.CONFIG_SIGN_AUTHN, false);
+        jksLocation		= sysConfig.getString(ServicesSAMLConstant.CONFIG_SP_CERT);
+        jksAlias		= sysConfig.getString(ServicesSAMLConstant.CONFIG_SP_CERT_ALIAS);
+        jksPassword		= sysConfig.getString(ServicesSAMLConstant.CONFIG_SP_STORE_PASS);
+        String strType	= sysConfig.getString(ServicesSAMLConstant.CONFIG_SP_STORE_TYPE);        
+        storeType = (strType != null && !strType.isEmpty())? strType : storeType;
+        
+        if(_logger.isDebugEnabled()) {
+			_logger.debug(String.format("LEAVING method %s (returns: %s)", "setKeystoreInformation", "void"));
+		}
 	}
 	
 	private SAMLConfig getSamlConfig() throws GeneralException {
+		if(_logger.isDebugEnabled()) {
+			_logger.debug(String.format("ENTERING method %s()", "getSamlConfig"));
+		}
 		SailPointContext context = getContext();
 		Configuration samlConfig = context.getObjectByName(Configuration.class, "SAML");
 		Attributes<String,Object> attributes = samlConfig.getAttributes();
 		
 		SAMLConfig config = (SAMLConfig) attributes.get("IdentityNow");
 		
+		if(_logger.isDebugEnabled()) {
+			_logger.debug(String.format("LEAVING method %s (returns: %s)", "getSamlConfig", config));
+		}
 		return config;
 	}
 	
 	private String returnSessionIndexFromDb(String principal) throws GeneralException, SQLException {
+		if(_logger.isDebugEnabled()) {
+			_logger.debug(String.format("ENTERING method %s(principal = %s)", "returnSessionIndexFromDb", principal));
+		}
 		String result = null;
 		Connection connection = getConnection();
 		PreparedStatement prepStatement = connection.prepareStatement(SQL_SESSION_QUERY);
@@ -119,6 +188,10 @@ public class SamlLogoutService extends BasePluginResource {
 		
 		rs.close();
 		prepStatement.close();
+		
+		if(_logger.isDebugEnabled()) {
+			_logger.debug(String.format("LEAVING method %s (returns: %s)", "returnSessionIndexFromDb", result));
+		}
 		return result;
 	}
 }
